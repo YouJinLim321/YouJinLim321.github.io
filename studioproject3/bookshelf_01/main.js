e706697efd497da7a498a1bcefc62b2c6fe73666
You Jin <70587051+YouJinLim321@users.noreply.github.com>
2021-04-08 15:16:46 +0900

    sd
    f

--- studioproject3/bookshelf_01/main.js
+++ studioproject3/bookshelf_01/main.js
@@ -38,7 +38,8 @@ function gotAllCovers(err) {
 
   // call functions to log and show the books
   consoleLogCovers();
-  showCovers();
+  showCovers(covers);
+  addFilterEventListeners();
 }
 
 // just loop through the books and console.log them
@@ -49,250 +50,56 @@ function consoleLogCovers() {
   });
 }
 
-// loop through the books, create an h2 for each one, and add it to the page
-function showCovers() {
+function showCovers(array) {
   console.log("showCovers()");
-  covers.forEach((cover) => {
-    
-        //add song titles
-        //var songTitle = document.createElement("h1");
-        //songTitle.classList.add("song-title");
-        //songTitle.innerText = song.fields.song_title;
-        //document.body.append(songTitle);
 
-        var coverContainer = document.createElement("div");
-        coverContainer.classList.add("cover-container");
-        document.querySelector(".covers").append(coverContainer);
+  // create an array of .cover-container elements out of the array passed as an argument to this function
+  const coverContainers = array.map((cover) => {
+    const coverContainer = document.createElement("div");
+    coverContainer.classList.add("cover-container");
 
-        //add song titles to our song container
-        var coverYear = document.createElement("h1");
-        coverYear.classList.add("cover-year");
-        
+    const coverImage = document.createElement("img");
+    coverImage.classList.add("cover-image");
+    coverImage.src = cover.fields.cover_image[0].url;
 
-       //add artists to our song container
-        var coverHair = document.createElement("h1");
-       coverHair.classList.add("cover-hair");
-      
-
-       //add description to our song container
-       var coverMakeup = document.createElement("p");
-        coverMakeup.classList.add("cover-makeup");
-        
-
-        var coverClothing = document.createElement("p");
-        coverClothing.classList.add("cover-clothing");
-        
-
-        var coverBackground = document.createElement("p");
-        coverBackground.classList.add("cover-background");
-        
-
-       //add image to our song container
-        var coverImage = document.createElement("img");
-        coverImage.classList.add("cover-image");
-        coverImage.src = cover.fields.cover_image[0].url;
-        coverContainer.append(coverImage);
-
-        //add event listener
-        //when user clicks on song container
-        //image and description will appear or disappear
-        coverContainer.addEventListener("click", function(){
-        coverDescription.classList.toggle("active");
-        coverImage.classList.toggle("active");
-
-
-        // get genre field from airtable
-        // loop through the array and add each genre as 
-        // a class to the song container
-
-        var coverGenre = cover.fields.genre;
-        coverGenre.forEach(function(genre) {
-          coverContainer.classList.add(genre)
-        });
-
-        //add event listener to our filter
-        // to add an active class to our song
-
-        var filterCurly = document.querySelector('.curly');
-        filterCurly.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("curly")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-      });
-
-        var filterStraight = document.querySelector('.straight');
-        filterStraight.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("straight")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-        var filterDreamy = document.querySelector('.dreamy');
-        filterDreamy.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("dreamy")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-        var filterLovely = document.querySelector('.lovely');
-        filterLovely.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("lovely")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-        var filterChic = document.querySelector('.chic');
-        filterChic.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("chic")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-        var filterPowerful = document.querySelector('.powerful');
-        filterPowerful.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("powerful")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-        var filterLonely = document.querySelector('.lonely');
-        filterLonely.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("lonely")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-        var filterSoleless = document.querySelector('.soleless');
-        filterSoleless.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("soleless")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-        var filterVintage = document.querySelector('.vintage');
-        filterVintage.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("vintage")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-        var filterSexy = document.querySelector('.sexy');
-        filterSexy.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("sexy")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-        var filterPlayful = document.querySelector('.playful');
-        filterPlayful.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("playful")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-        var filterWarm = document.querySelector('.warm');
-        filterWarm.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("warm")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-        var filterNeutral = document.querySelector('.neutral');
-        filterNeutral.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("neutral")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-       });
-
-        var filterBright = document.querySelector('.bright');
-        filterBright.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("bright")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-         var filterCool = document.querySelector('.cool');
-        filterCool.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("cool")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-         var filterDark = document.querySelector('.dark');
-        filterDark.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("dark")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-         var filterCity = document.querySelector('.city');
-        filterCity.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("city")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
-         var filterNature = document.querySelector('.nature');
-        filterNature.addEventListener("click", function() {
-
-          if(coverContainer.classList.contains("nature")) {
-            coverContainer.style.background = "red";
-          } else {
-          coverContainer.style.background = "white";
-        }
-        });
-
+    coverContainer.append(coverImage);
+    return coverContainer;
   });
-});
+
+  // appends coverContainers to .covers all at once.
+  // the append() method lets you append multiple elements simultaneously if you provide a comma-separated list inside the parentheses.
+  // for example: document.body.append(element1, element2, element3). it is like writing an array without the brackets.
+  // that is what the ... is for. it strips the brackets out of the array when we pass it as an argument to a function
+  document.querySelector(".covers").append(...coverContainers);
+}
+
+// function to erase the content of .covers
+function clearCovers() {
+  const allCovers = document.querySelector(".covers");
+  while (allCovers.childNodes.length > 0) {
+    allCovers.removeChild(allCovers.firstChild);
+  }
+}
+
+// add filter functionality to every .filter-item
+function addFilterEventListeners() {
+  const filterItems = document.querySelectorAll(".filter-item");
+  filterItems.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      // remove all covers first
+      clearCovers();
+
+      // event.target means the element being clicked on. note that you need event as an argument in the event listener callback, like so: (event) => {}
+
+      // parentNode means the parent of the element. in this case, the parentNode of button.filter-item is li.
+      // however, we want to get the 'data-category' attribute of the ul that is the parent of the li, so we call parentNode twice
+      const category = event.target.parentNode.parentNode.getAttribute("data-category");
+      const value = event.target.getAttribute("data-value");
+
+      // the filter() method lets us filter an array according to a condition. so if we do covers.filter((cover) => cover.fields.background[0] === "city") then it will return an array of covers with a city background
+      // using brackets, we can access the property of an object with a variable
+      // then we pass the filtered array to showCovers()
+      showCovers(covers.filter((cover) => cover.fields[category][0] === value));
+    }, false);
+  });
 }

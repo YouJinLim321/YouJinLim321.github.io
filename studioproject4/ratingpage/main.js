05cb90ecbff6bfd3f3de35e9d31d320542156271
You Jin <70587051+YouJinLim321@users.noreply.github.com>
2021-05-03 15:53:08 +0900

    d
    d

--- studioproject4/ratingpage/main.js
+++ studioproject4/ratingpage/main.js
@@ -1,21 +1,101 @@
-const root = document.getElementById("root");
-const ratingField = document.getElementById("rating-field");
-const submitButton = document.getElementById("submit-button");
+var emotionsArray = {
 
-const message = document.createElement("h1");
-message.className = "heading";
+    angry:"&#x1F620;",
 
-ratingField.addEventListener("change", (event) => {
-	if (Number(event.target.value) < 3) {
-		message.textContent = "We\u2019re sorry to hear that.";
-	} else if (Number(event.target.value) === 5) {
-		message.textContent = "Glad you liked it!";
-	} else {
-		message.textContent = "Thank you!";
-	}
+    disappointed:"&#x1F61E;",
+
+    meh:"&#x1F610;",
+
+    happy:"&#x1F60A;",
+
+    smile:"&#x1F603;",
+
+    wink:"&#x1F609;",
+
+    laughing:"&#x1F606;",
+
+    inLove:"&#x1F60D;",
+
+    heart:"&#x2764;",
+
+    crying:"&#x1F622;",
+
+    star:"&#x2B50;",
+};
+
+var emotionsArray = ['angry','disappointed','meh','happy','inLove'];
+
+$("#myRating").emotionsRating({
+
+  emotions: emotionsArray
+
 });
 
-submitButton.addEventListener("click", () => {
-	ratingField.style.display = "none";
-	root.appendChild(message);
+    $("#myRating").emotionsRating({
+
+ 
+
+  // background emoji
+
+  bgEmotion:"happy",
+
+
+  // number of emoji
+
+  count: 5,
+
+
+  // color of emoji
+
+  // gold, red, blue, green, black,
+
+  // brown, pink, purple, orange
+
+  color:"red",
+
+ 
+
+  // initial rating value
+
+  initialRating: 4
+
+ 
+
+  // size of emoji
+
+  emotionSize: 30,
+
+ 
+
+  // input name
+
+  inputName:"ratings[]",
+
+ 
+
+  // callback
+
+  emotionOnUp<a href="https://www.jqueryscript.net/time-clock/">date</a>:null,
+
+ 
+
+  // if is disabled?
+
+  disabled:false,
+
+ 
+
+  // enable use of images as emoji
+
+  useCustomEmotions:false,
+
+ 
+
+  // if you want to process the images
+
+  transformImages:false
+
+   
+
 });
+
